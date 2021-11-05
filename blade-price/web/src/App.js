b8fb93617ea4b60e190fde87dc0e20fb69f41ae4
Syngon <naotivecriatividadepraisso@gmail.com>
2021-11-04 22:16:24 -0300

    frontend strip done

--- blade-price/web/src/App.js
+++ blade-price/web/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+
+
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
-import { useAuth0 } from '@auth0/auth0-react';
+import StripeContainer from './components/StripeContainer';
+import fotoProduto from './assets/jarlesfemea.png'
+
+
+
+
+
 
 function App() {
   const { isLoading } = useAuth0();
+  const [showItem, setShowItem] = useState(false);
 
   if (isLoading) return <div>Loading...</div>
 
@@ -15,6 +25,19 @@ function App() {
       <LoginButton />
       <LogoutButton />
       <Profile />
+      <div className="stripe">
+        <h1> Produto </h1>
+        {showItem ? 
+          <StripeContainer/> : 
+          <>
+            <h3>$10,00</h3>
+            <img src={fotoProduto} alt="Foto do produto"/>
+            <button onClick={() => setShowItem(true)}>
+              Purchase product
+            </button>
+          </> 
+        }
+      </div>
     </>
   );
 }
